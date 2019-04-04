9a3fe0bfded8ef035ab262ca2f47f7761e2159e0
ilabstaff <ilabstaff@172.16.116.77>
2019-04-03 17:59:19 -0700

    riddler

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -9,11 +9,11 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-		int score=0"
+		int score=0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String input = JOptionPane.showInputDialog("What has 4 eye but can't see?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		JOptionPane.showMessageDialog(null,"correct it is "+input);
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
